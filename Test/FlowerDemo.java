cf9999a0216c9ce2fcddafca48d803cdbd00dc61
anoop <anoop@gmail.com>
2021-11-01 12:35:37 +0530

    Initial Commit

---
+++ Test/FlowerDemo.java
@@ -0,0 +1,33 @@
+package Test;
+//ADD A METHOD displayFlower WHICH DISPLAYS COUNTRIES NAME AND NATIONAL FLOWERS. 
+//CONTINENT NAME IS PASSED AS AN ARGUMENT.
+//YOU MAY USE THE FOLLOWING SAMPLE DATA AS INPUT
+public class FlowerDemo {
+	
+	public static void displayFlower(String display) {
+		
+		if(display == "Asia") {
+			 System.out.println("Continent Name: Asia");
+			 System.out.println("Country Name: India and National Flower : LOTUS");
+			 System.out.println("Country Name: Pakistan and National Flower : JASMINE");
+			 System.out.println("Country Name: Nepal and National Flower : LALI GURANS");
+			}
+		else if(display == "Europe") {
+			 System.out.println("Continent Name: Europe");
+			 System.out.println("Country Name: GERMANY and National Flower : CORN FLOWER");
+			 System.out.println("Country Name: FRANCE and National Flower : LILY");
+			 System.out.println("Country Name: GREECE and National Flower : ACANTHUS MOLLIS");
+		}
+		else
+		{
+			 System.out.println("Continent Name: Africa");
+			 System.out.println("Country Name : EGYPT and National Flower : BLUE LOTUS");
+			 System.out.println("Country Name : NIGERIA and National Flower : YELLOW TRUMPET ");
+			 System.out.println("Country Name : KENYA and National Flower : ORCHID");
+		}
+	}
+	
+	public static void main(String[] args) {
+		displayFlower("Asia");
+	}
+}
